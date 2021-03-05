3d7d1514cc6d6ffa2a44e872e3d2a8b57ceb5047
Fusion-Flux <71662169+Fusion-Flux@users.noreply.github.com>
2021-03-05 16:59:15 -0600

    PortalGun V1

--- src/main/java/com/fusionflux/fluxtech/items/PortalGun.java
+++ src/main/java/com/fusionflux/fluxtech/items/PortalGun.java
@@ -5,7 +5,7 @@ import com.qouteall.immersive_portals.api.PortalAPI;
 import com.qouteall.immersive_portals.portal.GeometryPortalShape;
 import com.qouteall.immersive_portals.portal.Portal;
 import com.qouteall.immersive_portals.portal.PortalManipulation;
-import com.sun.org.apache.xml.internal.dtm.Axis;
+
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
@@ -15,6 +15,7 @@ import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.hit.HitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
+import net.minecraft.util.math.Quaternion;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 
@@ -28,6 +29,8 @@ public class PortalGun extends Item {
     public static Portal portal2;
     public Vec3d direction1;
     public Vec3d direction2;
+    public Quaternion quion1;
+    public Quaternion quion2;
 
 
     public PortalGun(Settings settings) {
@@ -41,13 +44,13 @@ public class PortalGun extends Item {
             if (hitResult2.getType() == HitResult.Type.BLOCK) {
                 blockPos2 = ((BlockHitResult) hitResult2).getBlockPos();
                 direction2 = new Vec3d(((BlockHitResult) hitResult2).getSide().getVector().getX(), ((BlockHitResult) hitResult2).getSide().getVector().getY(), ((BlockHitResult) hitResult2).getSide().getVector().getZ());
-
-            }
+                quion1 = new Quaternion(((BlockHitResult)hitResult2).getSide().getOpposite().getRotationQuaternion());            }
         } else if (flipflop == 0) {
             HitResult hitResult = user.raycast(128.0D, 0.0F, false);
             if (hitResult.getType() == HitResult.Type.BLOCK) {
                 blockPos = ((BlockHitResult) hitResult).getBlockPos();
                 direction1 = new Vec3d(((BlockHitResult) hitResult).getSide().getVector().getX(), ((BlockHitResult) hitResult).getSide().getVector().getY(), ((BlockHitResult) hitResult).getSide().getVector().getZ());
+                quion2 = new Quaternion(((BlockHitResult)hitResult).getSide().getOpposite().getRotationQuaternion());
 
             }
         }
@@ -62,35 +65,44 @@ public class PortalGun extends Item {
 
             portal = Portal.entityType.create(world);
             assert portal != null;
-            portal.setOriginPos(new Vec3d(blockPos.getX() + .5 + (direction1.x / 1.96), blockPos.getY(), blockPos.getZ() + .5 + (direction1.z / 1.96)));
+            portal.setOriginPos(new Vec3d(blockPos.getX() + .5 + (direction1.x / 1.99), blockPos.getY()+ (direction1.y*1.01), blockPos.getZ() + .5 + (direction1.z / 1.99)-(direction2.y/2)));
             portal.setDestinationDimension(World.OVERWORLD);
             portal.setOrientationAndSize(
-                    new Vec3d(direction1.z, direction1.y, -direction1.x), //axisW
-                    new Vec3d(0, 1, 0), //axisH
+                    new Vec3d(direction1.z+direction1.y, 0, -(direction1.x)),//axisW
+                    new Vec3d(0, direction1.z+direction1.x, -direction1.y),//axisH
                     1, 2
             );
 
+
             portal2 = Portal.entityType.create(world);
             assert portal2 != null;
-            portal2.setOriginPos(new Vec3d(blockPos2.getX() + .5 + (direction2.x / 1.96), blockPos2.getY(), blockPos2.getZ() + .5 + (direction2.z / 1.96)));
+            portal2.setOriginPos(new Vec3d(blockPos2.getX() + .5 + (direction2.x / 1.99), blockPos2.getY()+ (direction2.y*1.01), blockPos2.getZ() + .5 + (direction2.z / 1.99)-(direction2.y/2)));
+            //portal2.setDestination(new Vec3d(blockPos2.getX() + .5 + (direction2.x / 1.96), blockPos2.getY(), blockPos2.getZ() + .5 + (direction2.z / 1.96)));
             portal2.setDestinationDimension(World.OVERWORLD);
             portal2.setOrientationAndSize(
-                    new Vec3d(direction2.z, direction2.y, -direction2.x),//axisW
-                    new Vec3d(0, 1, 0),//axisH
+                    new Vec3d(direction2.z+direction2.y, 0, -(direction2.x)),//axisW
+                    new Vec3d(0, direction2.z+direction2.x, -direction2.y),//axisH
                     1,//width
                     2//height
             );
-
         }
 
         if (blockPos != null && blockPos2 != null) {
+
+            portal.rotation=quion2;
+            portal2.rotation=quion1;
             portal.setDestination(portal2.getOriginPos());
             portal2.setDestination(portal.getOriginPos());
+            //portal = PortalAPI.createFlippedPortal(portal);
+            //portal2 = PortalAPI.createFlippedPortal(portal2);
             portal.world.spawnEntity(portal);
             portal2.world.spawnEntity(portal2);
 
             System.out.println("made portals");
         }
+
+
+
         if (bigflipflop == 0) {
             if (flipflop == 0) {
                 flipflop = 1;
---
+++ src/main/java/com/fusionflux/fluxtech/mixin/EntityMixin.java
@@ -0,0 +1,80 @@
+package com.fusionflux.fluxtech.mixin;
+
+import com.fusionflux.fluxtech.accessor.EnduriumToucher;
+import com.fusionflux.fluxtech.blocks.FluxTechBlocks;
+import com.fusionflux.fluxtech.config.FluxTechConfig2;
+import com.fusionflux.fluxtech.entity.EntityAttachments;
+import it.unimi.dsi.fastutil.objects.Object2DoubleMap;
+import net.minecraft.entity.*;
+import net.minecraft.entity.data.DataTracker;
+import net.minecraft.entity.data.TrackedData;
+import net.minecraft.entity.data.TrackedDataHandlerRegistry;
+import net.minecraft.entity.passive.FoxEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.vehicle.BoatEntity;
+import net.minecraft.fluid.Fluid;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvent;
+import net.minecraft.sound.SoundEvents;
+import net.minecraft.tag.Tag;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+import net.minecraft.util.math.MathHelper;
+import net.minecraft.util.math.Vec3d;
+import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(Entity.class)
+public abstract class EntityMixin implements EntityAttachments, EnduriumToucher {
+
+    private static final TrackedData<Boolean> IS_ROLLING = DataTracker.registerData(Entity.class, TrackedDataHandlerRegistry.BOOLEAN);
+    private static final TrackedData<Direction> DIRECTION = DataTracker.registerData(Entity.class, TrackedDataHandlerRegistry.FACING);
+
+
+    public double maxFallSpeed = 0;
+
+    @Override
+    public double getMaxFallSpeed() {
+        return maxFallSpeed;
+    }
+
+    @Override
+    public void setMaxFallSpeed(double maxFallSpeed) {
+        this.maxFallSpeed = maxFallSpeed;
+    }
+
+    @Shadow public World world;
+
+    @Shadow public abstract BlockPos getBlockPos();
+
+    @Inject(method = "tick", at = @At("HEAD"), cancellable = true)
+    public void tick(CallbackInfo ci) {
+            if(maxFallSpeed == 10 && world.getBlockState(this.getBlockPos()).getBlock() == FluxTechBlocks.PROPULSION_GEL){
+                maxFallSpeed = 10;
+            }else{
+                if(maxFallSpeed>0){
+                    maxFallSpeed=maxFallSpeed-1;
+                }
+            }
+
+    }
+
+
+
+
+
+    /*@Inject(method = "calculateDimensions", at = @At("TAIL"))
+public void calculateDimensions(CallbackInfo ci){
+        EntityPose entityPose2 = this.getPose();
+        EntityDimensions entityDimensions3 = this.getDimensions(entityPose2);
+        this.standingEyeHeight = this.getEyeHeight(entityPose2, entityDimensions3)-1;
+    }*/
+}
