38032b4d1e01bd1be818ff4eb129bcc06418bd9a
Fusion-Flux <71662169+Fusion-Flux@users.noreply.github.com>
2021-03-05 02:38:33 -0600

    PortalGun V1

--- src/main/java/com/fusionflux/fluxtech/items/PortalGun.java
+++ src/main/java/com/fusionflux/fluxtech/items/PortalGun.java
@@ -1,6 +1,11 @@
 package com.fusionflux.fluxtech.items;
 
+
+import com.qouteall.immersive_portals.api.PortalAPI;
+import com.qouteall.immersive_portals.portal.GeometryPortalShape;
 import com.qouteall.immersive_portals.portal.Portal;
+import com.qouteall.immersive_portals.portal.PortalManipulation;
+import com.sun.org.apache.xml.internal.dtm.Axis;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
@@ -9,6 +14,7 @@ import net.minecraft.util.TypedActionResult;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.hit.HitResult;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 
@@ -18,8 +24,11 @@ public class PortalGun extends Item {
     public int bigflipflop = 1;
     public BlockPos blockPos;
     public BlockPos blockPos2;
-    public Portal portal;
-    public Portal portal2;
+    public static Portal portal;
+    public static Portal portal2;
+    public Vec3d direction1;
+    public Vec3d direction2;
+
 
     public PortalGun(Settings settings) {
         super(settings);
@@ -31,11 +40,15 @@ public class PortalGun extends Item {
             HitResult hitResult2 = user.raycast(128.0D, 0.0F, false);
             if (hitResult2.getType() == HitResult.Type.BLOCK) {
                 blockPos2 = ((BlockHitResult) hitResult2).getBlockPos();
+                direction2 = new Vec3d(((BlockHitResult) hitResult2).getSide().getVector().getX(), ((BlockHitResult) hitResult2).getSide().getVector().getY(), ((BlockHitResult) hitResult2).getSide().getVector().getZ());
+
             }
         } else if (flipflop == 0) {
             HitResult hitResult = user.raycast(128.0D, 0.0F, false);
             if (hitResult.getType() == HitResult.Type.BLOCK) {
                 blockPos = ((BlockHitResult) hitResult).getBlockPos();
+                direction1 = new Vec3d(((BlockHitResult) hitResult).getSide().getVector().getX(), ((BlockHitResult) hitResult).getSide().getVector().getY(), ((BlockHitResult) hitResult).getSide().getVector().getZ());
+
             }
         }
 
@@ -49,34 +62,35 @@ public class PortalGun extends Item {
 
             portal = Portal.entityType.create(world);
             assert portal != null;
-            portal.setOriginPos(new Vec3d(blockPos.getX(), blockPos.getY(), blockPos.getZ()));
+            portal.setOriginPos(new Vec3d(blockPos.getX() + .5 + (direction1.x / 1.96), blockPos.getY(), blockPos.getZ() + .5 + (direction1.z / 1.96)));
             portal.setDestinationDimension(World.OVERWORLD);
-            portal.setDestination(new Vec3d(blockPos2.getX(), blockPos2.getY(), blockPos2.getZ()));
             portal.setOrientationAndSize(
-                    new Vec3d(1, 0, 0), //axisW
+                    new Vec3d(direction1.z, direction1.y, -direction1.x), //axisW
                     new Vec3d(0, 1, 0), //axisH
-                    4, 4
+                    1, 2
             );
 
             portal2 = Portal.entityType.create(world);
             assert portal2 != null;
-            portal2.setOriginPos(new Vec3d(blockPos2.getX(), blockPos2.getY(), blockPos2.getZ()));
+            portal2.setOriginPos(new Vec3d(blockPos2.getX() + .5 + (direction2.x / 1.96), blockPos2.getY(), blockPos2.getZ() + .5 + (direction2.z / 1.96)));
             portal2.setDestinationDimension(World.OVERWORLD);
-            portal2.setDestination(new Vec3d(blockPos.getX(), blockPos.getY(), blockPos.getZ()));
             portal2.setOrientationAndSize(
-                    new Vec3d(1, 0, 0),//axisW
+                    new Vec3d(direction2.z, direction2.y, -direction2.x),//axisW
                     new Vec3d(0, 1, 0),//axisH
-                    4,//width
-                    4//height
+                    1,//width
+                    2//height
             );
+
         }
 
         if (blockPos != null && blockPos2 != null) {
+            portal.setDestination(portal2.getOriginPos());
+            portal2.setDestination(portal.getOriginPos());
             portal.world.spawnEntity(portal);
             portal2.world.spawnEntity(portal2);
+
             System.out.println("made portals");
         }
-
         if (bigflipflop == 0) {
             if (flipflop == 0) {
                 flipflop = 1;
