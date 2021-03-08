db358bcbbe11c3df06a67fb858b77810ea6ebc9a
Fusion Flux <c4gamer2.0@gmail.com>
2021-03-08 00:32:13 -0600

    test for rotations

--- src/main/java/com/fusionflux/fluxtech/items/PortalGun.java
+++ src/main/java/com/fusionflux/fluxtech/items/PortalGun.java
@@ -2,8 +2,11 @@ package com.fusionflux.fluxtech.items;
 
 
 import com.qouteall.immersive_portals.api.PortalAPI;
+import com.qouteall.immersive_portals.my_util.DQuaternion;
+import com.qouteall.immersive_portals.portal.GeometryPortalShape;
 import com.qouteall.immersive_portals.portal.Portal;
 
+import com.qouteall.immersive_portals.portal.PortalManipulation;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
@@ -16,6 +19,9 @@ import net.minecraft.util.math.Direction;
 import net.minecraft.util.math.Quaternion;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
+
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 
 public class PortalGun extends Item {
@@ -30,6 +36,12 @@ public class PortalGun extends Item {
     public Direction testdirection;
     public Quaternion quion1;
     public Quaternion quion2;
+    public Quaternion quion3;
+    public Quaternion quion4;
+    public float yqvalue;
+    public float wqvalue;
+    public float yqvalue2;
+    public float wqvalue2;
 public int testv =0;
 
 
@@ -37,29 +49,32 @@ public int testv =0;
         super(settings);
     }
 
+    public static Quaternion convertQuaternion(double x, double y, double z, double w) {
+        return new DQuaternion(x, y, z, w).toMcQuaternion();
+    }
+
     @Override
     public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
-        if (user.isSneaking()) {
-            HitResult hitResult2 = user.raycast(128.0D, 0.0F, false);
-            if (hitResult2.getType() == HitResult.Type.BLOCK) {
-                blockPos2 = ((BlockHitResult) hitResult2).getBlockPos();
-                direction2 = new Vec3d(((BlockHitResult) hitResult2).getSide().getVector().getX(), ((BlockHitResult) hitResult2).getSide().getVector().getY(), ((BlockHitResult) hitResult2).getSide().getVector().getZ());
-                quion1 = new Quaternion(((BlockHitResult)hitResult2).getSide().getOpposite().getRotationQuaternion());
-                //quion1.set(((BlockHitResult)hitResult2).getSide().getOpposite().getRotationQuaternion().getX()+90,((BlockHitResult)hitResult2).getSide().getOpposite().getRotationQuaternion().getY(),((BlockHitResult)hitResult2).getSide().getOpposite().getRotationQuaternion().getZ(),((BlockHitResult)hitResult2).getSide().getOpposite().getRotationQuaternion().getW());
-            }
-        } else if (!user.isSneaking()) {
-            HitResult hitResult = user.raycast(128.0D, 0.0F, false);
+        HitResult hitResult = user.raycast(128.0D, 0.0F, false);
+        if (!user.isSneaking()) {
             if (hitResult.getType() == HitResult.Type.BLOCK) {
                 blockPos = ((BlockHitResult) hitResult).getBlockPos();
                 direction1 = new Vec3d(((BlockHitResult) hitResult).getSide().getVector().getX(), ((BlockHitResult) hitResult).getSide().getVector().getY(), ((BlockHitResult) hitResult).getSide().getVector().getZ());
-                testdirection = (((BlockHitResult) hitResult).getSide());
-                quion2 = testdirection.getRotationQuaternion();
-
                 //quion2 = new Quaternion(((BlockHitResult)hitResult).getSide().getOpposite().getRotationQuaternion());
-
+                yqvalue2 = ((BlockHitResult)hitResult).getSide().getOpposite().getRotationQuaternion().getY();
+                wqvalue2 = ((BlockHitResult)hitResult).getSide().getOpposite().getRotationQuaternion().getW();
             }
         }
-
+        if (user.isSneaking()) {
+            if (hitResult.getType() == HitResult.Type.BLOCK) {
+                blockPos2 = ((BlockHitResult) hitResult).getBlockPos();
+                direction2 = new Vec3d(((BlockHitResult) hitResult).getSide().getVector().getX(), ((BlockHitResult) hitResult).getSide().getVector().getY(), ((BlockHitResult) hitResult).getSide().getVector().getZ());
+                //quion1 = new Quaternion(Direction.DOWN.getRotationQuaternion().getX(),((BlockHitResult)hitResult).getSide().getOpposite().getRotationQuaternion().getY(),((BlockHitResult)hitResult).getSide().getOpposite().getRotationQuaternion().getZ(),((BlockHitResult)hitResult).getSide().getOpposite().getRotationQuaternion().getW());
+                //quion1=Direction.SOUTH.getRotationQuaternion();
+                yqvalue = ((BlockHitResult)hitResult).getSide().getOpposite().getRotationQuaternion().getY();
+                wqvalue = ((BlockHitResult)hitResult).getSide().getOpposite().getRotationQuaternion().getW();
+            }
+        }
         if (blockPos != null && blockPos2 != null) {
 
             if (portalholder1 != null && portalholder2 != null) {
@@ -75,40 +90,58 @@ public int testv =0;
             portal2.setDestinationDimension(World.OVERWORLD);
             System.out.println(direction2);
             portal2.setOrientationAndSize(
-                    new Vec3d(direction2.z+direction2.y, 0, (direction2.x)),//axisW,//axisW
-                    new Vec3d(0, direction2.z+direction2.x, direction2.y),//axisH
+                    new Vec3d(direction2.z+direction2.y, 0, -(direction2.x)),//axisW,//axisW
+                    new Vec3d(0, -1, direction2.y),//axisH
                     1,//width
                     2//height
             );
             portalholder1=PortalAPI.createFlippedPortal(portal2);
             portal2.setOrientationAndSize(
-                    new Vec3d(direction1.z+direction1.y, 0, (direction1.x)),//axisW,//axisW
-                    new Vec3d(0, direction1.z+direction1.x, direction1.y),//axisH
+                    new Vec3d(direction1.z+direction1.y, 0, -(direction1.x)),//axisW,//axisW
+                    new Vec3d(0, -1, direction1.y),//axisH
                     1, 2
             );
             portalholder2=PortalAPI.createReversePortal(portal2);
-            /*if (testv == 0) {
-               // quion1 = portalholder1.getRotation();
-                //quion2 = portalholder2.getRotation();
-            }
-            testv++;
-            if(testv==2){
-                testv=0;
-            }
-            //portalholder1.rotation = quion2;
-            //portalholder2.rotation = quion1;*/
+
+            quion1= PortalManipulation.getPortalOrientationQuaternion(new Vec3d(direction2.z+direction2.y, 0, -(direction2.x)),new Vec3d(0, -1, direction2.y)).toMcQuaternion();
+            quion2= PortalManipulation.getPortalOrientationQuaternion(new Vec3d(direction1.z+direction1.y, 0, -(direction1.x)),new Vec3d(0, -1, direction1.y)).toMcQuaternion();
+
+            quion3 =convertQuaternion(quion1.getX(),1,quion1.getZ(),0);
+            quion4 =convertQuaternion(quion2.getX(),1,quion2.getZ(),0);
         }
 
         if (blockPos != null && blockPos2 != null) {
+            portalholder1.rotation = quion4;
+            portalholder2.rotation = quion3;
+            makeRoundPortal(portalholder1);
+            makeRoundPortal(portalholder2);
             if (!user.world.isClient()) {
                 portalholder1.world.spawnEntity(portalholder1);
                 portalholder2.world.spawnEntity(portalholder2);
-                portalholder1.reloadAndSyncToClient();
-                portalholder2.reloadAndSyncToClient();
+
             }
             System.out.println("made portals");
         }
 
         return TypedActionResult.pass(user.getStackInHand(hand));
     }
+    public static void makeRoundPortal(Portal portal) {
+        GeometryPortalShape shape = new GeometryPortalShape();
+        final int triangleNum = 30;
+        double twoPi = Math.PI * 2;
+        shape.triangles = IntStream.range(0, triangleNum)
+                .mapToObj(i -> new GeometryPortalShape.TriangleInPlane(
+                        0, 0,
+                        portal.width * 0.5 * Math.cos(twoPi * ((double) i) / triangleNum),
+                        portal.height * 0.5 * Math.sin(twoPi * ((double) i) / triangleNum),
+                        portal.width * 0.5 * Math.cos(twoPi * ((double) i + 1) / triangleNum),
+                        portal.height * 0.5 * Math.sin(twoPi * ((double) i + 1) / triangleNum)
+                )).collect(Collectors.toList());
+        portal.specialShape = shape;
+        portal.cullableXStart = 0;
+        portal.cullableXEnd = 0;
+        portal.cullableYStart = 0;
+        portal.cullableYEnd = 0;
+    }
+
 }
