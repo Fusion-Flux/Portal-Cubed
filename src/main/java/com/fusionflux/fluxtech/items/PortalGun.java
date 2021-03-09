531a23980ace3adf0f61e99f41b10d853fa1736b
sailKiteV <sailKitev@gmail.com>
2021-03-09 12:27:22 -0500

    Temporary semi-working :irit: quaternion generalization. Includes backup implementation if we bork this one.

--- src/main/java/com/fusionflux/fluxtech/items/PortalGun.java
+++ src/main/java/com/fusionflux/fluxtech/items/PortalGun.java
@@ -26,8 +26,8 @@ public class PortalGun extends Item {
 
     private BlockPos blockPos;
     private BlockPos blockPos2;
-    public static Portal portalholder1;
-    public static Portal portalholder2;
+    public Portal portalholder1;
+    public Portal portalholder2;
     public Vec3d direction1;
     public Vec3d direction2;
     public float yqvalue;
@@ -39,120 +39,80 @@ public class PortalGun extends Item {
         super(settings);
     }
 
-    public void useLeft(World world, PlayerEntity user, Hand hand) {
-        useImpl(world, user, hand, true);
-    }
-
     @Override
     public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
-        return useImpl(world, user, hand, false);
-    }
-
-    public TypedActionResult<ItemStack> useImpl(World world, PlayerEntity user, Hand hand, boolean leftClick) {
-        if (!world.isClient) {
-            HitResult hitResult = user.raycast(128.0D, 0.0F, false);
-            if (hitResult.getType() == HitResult.Type.BLOCK) {
-                if (leftClick) {
-                    blockPos = ((BlockHitResult) hitResult).getBlockPos();
-                    direction1 = new Vec3d(((BlockHitResult) hitResult).getSide().getVector().getX(), ((BlockHitResult) hitResult).getSide().getVector().getY(), ((BlockHitResult) hitResult).getSide().getVector().getZ());
-                    yqvalue2 = ((BlockHitResult) hitResult).getSide().getOpposite().getRotationQuaternion().getY();
-                    wqvalue2 = ((BlockHitResult) hitResult).getSide().getOpposite().getRotationQuaternion().getW();
-                } else {
-                    blockPos2 = ((BlockHitResult) hitResult).getBlockPos();
-                    direction2 = new Vec3d(((BlockHitResult) hitResult).getSide().getVector().getX(), ((BlockHitResult) hitResult).getSide().getVector().getY(), ((BlockHitResult) hitResult).getSide().getVector().getZ());
-                    yqvalue = ((BlockHitResult) hitResult).getSide().getOpposite().getRotationQuaternion().getY();
-                    wqvalue = ((BlockHitResult) hitResult).getSide().getOpposite().getRotationQuaternion().getW();
-                }
-            }
-            if (blockPos != null && blockPos2 != null) {
-
-                Portal portalBase;
-
-                if (portalholder1 != null && portalholder2 != null) {
-                    portalholder1.kill();
-                    portalholder2.kill();
-                    // System.out.println("removed portals");
-                }
-
-                portalBase = Portal.entityType.create(world);
-
-                // Should never be null unless something is very wrong
-                assert portalBase != null;
-
-                portalBase.setOriginPos(new Vec3d(blockPos2.getX() + .5 + (direction2.x / 1.99), blockPos2.getY() + (direction2.y * 1.01), blockPos2.getZ() + .5 + (direction2.z / 1.99) - (direction2.y / 2)));
-                portalBase.setDestination(new Vec3d(blockPos.getX() + .5 + (direction1.x / 1.99), blockPos.getY() + (direction1.y * 1.01), blockPos.getZ() + .5 + (direction1.z / 1.99) - (direction1.y / 2)));
-                portalBase.setDestinationDimension(World.OVERWORLD);
-                portalBase.setOrientationAndSize(
-                        new Vec3d(direction2.z + direction2.y, 0, -(direction2.x)), //axisW
-                        new Vec3d(0, -1, direction2.y), //axisH
-                        1, //width
-                        2 //height
-                );
-                portalholder1 = PortalAPI.createFlippedPortal(portalBase);
-                portalBase.setOrientationAndSize(
-                        new Vec3d(direction1.z + direction1.y, 0, -(direction1.x)), //axisW
-                        new Vec3d(0, -1, direction1.y), //axisH
-                        1, 2
-                );
-                portalholder2 = PortalAPI.createReversePortal(portalBase);
-
-                // TODO
-                // get these to return quaternions for use in rotating
-                // Quaternion quion1;
-                // Quaternion quion2;
-                // quion1 = PortalManipulation.getPortalOrientationQuaternion(new Vec3d(direction2.z+direction2.y, 0, -(direction2.x)),new Vec3d(0, -1, direction2.y)).toMcQuaternion();
-                // quion2 = PortalManipulation.getPortalOrientationQuaternion(new Vec3d(direction1.z+direction1.y, 0, -(direction1.x)),new Vec3d(0, -1, direction1.y)).toMcQuaternion();
-
-
-                // TODO
-                // Un-hardcode these values
-                /*---------------------
-                Quaternion quion3 = new Quaternion( new Vector3f(portalholder1.axisH), (float)(portalholder2.axisW.dotProduct( portalholder1.axisW )/(portalholder1.axisW.length() * portalholder2.axisW.length())), true);
-                quion3.normalize();
-                Quaternion quion4 = new Quaternion( new Vector3f(portalholder2.axisH),  (float)(portalholder1.axisW.dotProduct( portalholder2.axisW )/(portalholder2.axisW.length() * portalholder1.axisW.length())), true);
-                quion4.normalize();
-                //--------------------*/
-
-                DQuaternion dQuion5 = PortalManipulation.getPortalOrientationQuaternion(portalholder1.axisW, portalholder1.axisH);
-                DQuaternion dQuion6 = PortalManipulation.getPortalOrientationQuaternion(portalholder2.axisW, portalholder2.axisH);
-                DQuaternion tempDQuion = dQuion5;
-
-                dQuion5 = ((dQuion5.getConjugated()).hamiltonProduct(dQuion6));
-                Quaternion quion5 = dQuion5.getNormalized().toMcQuaternion();
-
-                dQuion6 = ((dQuion6.getConjugated()).hamiltonProduct(tempDQuion));
-                Quaternion quion6 = dQuion6.getNormalized().toMcQuaternion();
-
-                //quion6.normalize();
-                //Quaternion quionTemp = quion5;
-
-                /*----------
-                quion5.hamiltonProduct( quion6 );
-                quion5.normalize();
-                quion6.conjugate();
-                quion6.hamiltonProduct( quionTemp );
-                quion6.normalize();
-                //--------*/
-
-                portalholder1.setRotationTransformation(quion5);
-                portalholder2.setRotationTransformation(quion6);
-                System.out.println(portalholder1.rotation);
-                System.out.println(portalholder2.rotation);
-
-
-                // commented out for now as it just fills the portal data with too much text
-                //makeRoundPortal(portalholder1);
-                //makeRoundPortal(portalholder2);
-
-                world.spawnEntity(portalholder1);
-                world.spawnEntity(portalholder2);
-                // System.out.println("made portals");
-
-                // sail stuff
-                portalholder1.reloadAndSyncToClient();
-                portalholder2.reloadAndSyncToClient();
+        HitResult hitResult = user.raycast(128.0D, 0.0F, false);
+        if (hitResult.getType() == HitResult.Type.BLOCK) {
+            if (!user.isSneaking()) {
+                blockPos = ((BlockHitResult) hitResult).getBlockPos();
+                direction1 = new Vec3d(((BlockHitResult) hitResult).getSide().getVector().getX(), ((BlockHitResult) hitResult).getSide().getVector().getY(), ((BlockHitResult) hitResult).getSide().getVector().getZ());
+                yqvalue2 = ((BlockHitResult)hitResult).getSide().getOpposite().getRotationQuaternion().getY();
+                wqvalue2 = ((BlockHitResult)hitResult).getSide().getOpposite().getRotationQuaternion().getW();
+            } else if (user.isSneaking()) {
+                blockPos2 = ((BlockHitResult) hitResult).getBlockPos();
+                direction2 = new Vec3d(((BlockHitResult) hitResult).getSide().getVector().getX(), ((BlockHitResult) hitResult).getSide().getVector().getY(), ((BlockHitResult) hitResult).getSide().getVector().getZ());
+                yqvalue = ((BlockHitResult)hitResult).getSide().getOpposite().getRotationQuaternion().getY();
+                wqvalue = ((BlockHitResult)hitResult).getSide().getOpposite().getRotationQuaternion().getW();
             }
         }
+        if (blockPos != null && blockPos2 != null && !world.isClient) {
+
+            Portal portalBase;
+
+            if (portalholder1 != null && portalholder2 != null) {
+                portalholder1.kill();
+                portalholder2.kill();
+                // System.out.println("removed portals");
+            }
+
+            portalBase = Portal.entityType.create(world);
+
+            // Should never be null unless something is very wrong
+            assert portalBase != null;
+
+            portalBase.setOriginPos(new Vec3d(blockPos2.getX() + .5 + (direction2.x / 1.99), blockPos2.getY() + (direction2.y * 1.01), blockPos2.getZ() + .5 + (direction2.z / 1.99)-(direction2.y / 2)));
+            portalBase.setDestination(new Vec3d(blockPos.getX() + .5 + (direction1.x / 1.99), blockPos.getY() + (direction1.y * 1.01), blockPos.getZ() + .5 + (direction1.z / 1.99)-(direction1.y / 2)));
+            portalBase.setDestinationDimension(World.OVERWORLD);
+            portalBase.setOrientationAndSize(
+                    new Vec3d(direction2.z+direction2.y, 0, -(direction2.x)), //axisW
+                    new Vec3d(0, -1, direction2.y), //axisH
+                    1, //width
+                    2 //height
+            );
+            portalholder1=PortalAPI.createFlippedPortal(portalBase);
+            portalBase.setOrientationAndSize(
+                    new Vec3d(direction1.z+direction1.y, 0, -(direction1.x)), //axisW
+                    new Vec3d(0, -1, direction1.y), //axisH
+                    1, 2
+            );
+            portalholder2=PortalAPI.createReversePortal(portalBase);
+
+            // Quaternion time
+            /*---------- old (and possibly most generalized?) quat code, keeping in case anything asplodes
+            DQuaternion dQuionIn1 = PortalManipulation.getPortalOrientationQuaternion( portalholder1.axisW.negate(), portalholder1.axisH ).getNormalized();
+            DQuaternion dQuionIn2 = PortalManipulation.getPortalOrientationQuaternion( portalholder2.axisW.negate(), portalholder2.axisH ).getNormalized();
+            DQuaternion dQuionOut1 = PortalManipulation.getPortalOrientationQuaternion( portalholder1.axisW, portalholder2.axisH ).getNormalized();
+            DQuaternion dQuionOut2 = PortalManipulation.getPortalOrientationQuaternion( portalholder2.axisW, portalholder2.axisH ).getNormalized();
+            ----------*/
+
+            /*--- this works for literally all regular upright cases except same-direction :irit: ---*/
+            Quaternion quion1 = DQuaternion.getRotationBetween( portalholder1.getNormal(), portalholder2.getNormal().negate() ).getNormalized().toMcQuaternion();
+            Quaternion quion2 = DQuaternion.getRotationBetween( portalholder2.getNormal(), portalholder1.getNormal().negate() ).getNormalized().toMcQuaternion();
+
+            portalholder1.setRotationTransformation(quion1);
+            portalholder2.setRotationTransformation(quion2);
+
+            // commented out for now as it just fills the portal data with too much text
+            //makeRoundPortal(portalholder1);
+            //makeRoundPortal(portalholder2);
+
+            world.spawnEntity(portalholder1);
+            world.spawnEntity(portalholder2);
+
+            portalholder1.reloadAndSyncToClient();
+            portalholder2.reloadAndSyncToClient();
+        }
+
         return TypedActionResult.pass(user.getStackInHand(hand));
     }
     public static void makeRoundPortal(Portal portal) {
