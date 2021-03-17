1912b1974c43ad30041336d0a1818f07825d6e87
Fusion Flux <c4gamer2.0@gmail.com>
2021-03-17 04:14:03 -0500

    fixed CC sounds

--- src/main/java/com/fusionflux/thinkingwithportatos/entity/CompanionCubeEntity.java
+++ src/main/java/com/fusionflux/thinkingwithportatos/entity/CompanionCubeEntity.java
@@ -3,13 +3,22 @@ package com.fusionflux.thinkingwithportatos.entity;
 import com.fusionflux.thinkingwithportatos.ThinkingWithPortatos;
 import com.fusionflux.thinkingwithportatos.items.ThinkingWithPortatosItems;
 import com.fusionflux.thinkingwithportatos.sound.ThinkingWithPortatosSounds;
+import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.sound.EntityTrackingSoundInstance;
+import net.minecraft.client.sound.SoundSystem;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.network.Packet;
+import net.minecraft.network.packet.s2c.play.PlaySoundFromEntityS2CPacket;
 import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvent;
 import net.minecraft.util.Identifier;
 import net.minecraft.world.GameRules;
 import net.minecraft.world.World;
+
+import java.util.Objects;
 
 public class CompanionCubeEntity extends CubeEntity {
     public static final Identifier SPAWN_PACKET = new Identifier(ThinkingWithPortatos.MODID, "companion_cube");
@@ -18,12 +27,14 @@ public class CompanionCubeEntity extends CubeEntity {
         super(entityType, world);
     }
 
+
     private int t = 1760;
     @Override
     public void tick() {
-        if(!this.world.isClient) {
+        if(this.world.isClient) {
             if (t == 1760) {
-                world.playSoundFromEntity(null,this, ThinkingWithPortatosSounds.COMPANION_CUBE_AMBIANCE_EVENT, SoundCategory.NEUTRAL,1F,1F);
+                MinecraftClient.getInstance().getSoundManager().play(new EntityTrackingSoundInstance(ThinkingWithPortatosSounds.COMPANION_CUBE_AMBIANCE_EVENT, this.getSoundCategory(),.0008F,1F,this));
+                //Objects.requireNonNull(this.getServer()).getPlayerManager().sendToAround(null,this.getPos().getX(),this.getPos().getY(),this.getPos().getZ(),.1,world.getRegistryKey(),new PlaySoundFromEntityS2CPacket(ThinkingWithPortatosSounds.COMPANION_CUBE_AMBIANCE_EVENT, this.getSoundCategory(),this,.00001F,1F));
                 t--;
 
             }
