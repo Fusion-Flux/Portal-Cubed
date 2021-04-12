a89595973ca4bbcf50a4f4c020820e69679158eb
Fusion Flux <c4gamer2.0@gmail.com>
2021-04-12 16:03:51 -0500

    added crafting recipies for the portal gun and its subitems

--- src/main/java/com/fusionflux/thinkingwithportatos/items/ThinkingWithPortatosItems.java
+++ src/main/java/com/fusionflux/thinkingwithportatos/items/ThinkingWithPortatosItems.java
@@ -17,6 +17,9 @@ public class ThinkingWithPortatosItems {
     public static final GelOrb GEL_ORB = new GelOrb(new FabricItemSettings().group(ThinkingWithPortatos.ThinkingWithPortatosGroup).maxCount(64));
     public static final Item LONG_FALL_BOOTS = new ArmorItem(FluxTechArmor, EquipmentSlot.FEET, new Item.Settings().group(ThinkingWithPortatos.ThinkingWithPortatosGroup).fireproof());
     public static final PortalGun PORTAL_GUN = new PortalGun(new FabricItemSettings().group(ThinkingWithPortatos.ThinkingWithPortatosGroup).maxCount(1).fireproof());
+    public static final PortalGun PORTAL_GUN_FRAME = new PortalGun(new FabricItemSettings().group(ThinkingWithPortatos.ThinkingWithPortatosGroup).maxCount(1).fireproof());
+    public static final PortalGun PORTAL_GUN_CASING = new PortalGun(new FabricItemSettings().group(ThinkingWithPortatos.ThinkingWithPortatosGroup).maxCount(1).fireproof());
+    public static final PortalGun MINI_BLACKHOLE = new PortalGun(new FabricItemSettings().group(ThinkingWithPortatos.ThinkingWithPortatosGroup).maxCount(1).fireproof());
     public static final PortalGun PORTAL_GUN_MODEL2 = new PortalGun(new FabricItemSettings().group(ThinkingWithPortatos.ThinkingWithPortatosGroup).maxCount(1).fireproof());
     public static final PaintGun PAINT_GUN = new PaintGun(new FabricItemSettings().group(ThinkingWithPortatos.ThinkingWithPortatosGroup).maxCount(1).fireproof());
     public static final SpawnEggItem CUBE = new SpawnEggItem(ThinkingWithPortatosEntities.CUBE, 1, 1, new FabricItemSettings().group(ThinkingWithPortatos.ThinkingWithPortatosGroup));
@@ -27,6 +30,9 @@ public class ThinkingWithPortatosItems {
             Registry.register(Registry.ITEM, new Identifier(ThinkingWithPortatos.MODID, "long_fall_boots"), LONG_FALL_BOOTS);
        // Registry.register(Registry.ITEM, new Identifier(ThinkingWithPortatos.MODID, "gel_orb"), GEL_ORB);
         Registry.register(Registry.ITEM, new Identifier(ThinkingWithPortatos.MODID, "portal_gun"), PORTAL_GUN);
+        Registry.register(Registry.ITEM, new Identifier(ThinkingWithPortatos.MODID, "portal_gun_frame"), PORTAL_GUN_FRAME);
+        Registry.register(Registry.ITEM, new Identifier(ThinkingWithPortatos.MODID, "portal_gun_casing"), PORTAL_GUN_CASING);
+        Registry.register(Registry.ITEM, new Identifier(ThinkingWithPortatos.MODID, "mini_blackhole"), MINI_BLACKHOLE);
         Registry.register(Registry.ITEM, new Identifier(ThinkingWithPortatos.MODID, "portal_gun_model2"), PORTAL_GUN_MODEL2);
        // Registry.register(Registry.ITEM, new Identifier(ThinkingWithPortatos.MODID, "paint_gun"), PAINT_GUN);
         Registry.register(Registry.ITEM, new Identifier(ThinkingWithPortatos.MODID, "cube"), CUBE);
