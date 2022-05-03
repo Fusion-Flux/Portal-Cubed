43ccb50e5301cc734e24c060ae6a4ed9b2df2b58
Fusion Flux <c4gamer2.0@gmail.com>
2022-05-02 21:26:29 -0500

    im 90% sure the only change here is that i gave the companion cube a dropped item

--- src/main/java/com/fusionflux/portalcubed/entity/CompanionCubeEntity.java
+++ src/main/java/com/fusionflux/portalcubed/entity/CompanionCubeEntity.java
@@ -1,8 +1,12 @@
 package com.fusionflux.portalcubed.entity;
 
+import com.fusionflux.portalcubed.items.PortalCubedItems;
 import com.fusionflux.portalcubed.sound.PortalCubedSounds;
 import net.minecraft.entity.EntityType;
+import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.mob.PathAwareEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.world.GameRules;
 import net.minecraft.world.World;
 
 public class CompanionCubeEntity extends StorageCubeEntity  {
@@ -11,6 +15,32 @@ public class CompanionCubeEntity extends StorageCubeEntity  {
     }
 
     private int t = 1500;
+
+    private float storedDamage = 0.0F;
+
+    @Override
+    public boolean damage(DamageSource source, float amount) {
+        if (this.isInvulnerableTo(source)) {
+            return false;
+        } else if (!this.world.isClient && !this.isRemoved()) {
+            this.storedDamage += amount;
+            //this.scheduleVelocityUpdate();
+            boolean bl = source.getAttacker() instanceof PlayerEntity && ((PlayerEntity) source.getAttacker()).getAbilities().creativeMode;
+            if (bl || this.storedDamage >= 20.0F) {
+                if (this.world.getGameRules().getBoolean(GameRules.DO_ENTITY_DROPS)) {
+                    // TODO
+                    this.dropItem(PortalCubedItems.COMPANION_CUBE);
+                }
+
+                this.discard();
+            }
+
+            return true;
+        } else {
+            return true;
+        }
+    }
+
 
     @Override
     public void tick() {
