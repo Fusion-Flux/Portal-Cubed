b2d70a0c7a1d540e6bf8ac58e0fec938d77aebea
Josiah (Gaming32) Glosson <soujournme@gmail.com>
2023-01-29 00:25:53 -0600

    Fix merge errors

--- src/main/java/com/fusionflux/portalcubed/mixin/client/ModelLoaderMixin.java
+++ src/main/java/com/fusionflux/portalcubed/mixin/client/ModelLoaderMixin.java
@@ -1,7 +1,12 @@
 package com.fusionflux.portalcubed.mixin.client;
 
-import java.util.Map;
-
+import com.fusionflux.portalcubed.PortalCubed;
+import com.fusionflux.portalcubed.client.render.model.block.EmissiveBakedModel;
+import net.minecraft.client.render.model.BakedModel;
+import net.minecraft.client.render.model.ModelBakeSettings;
+import net.minecraft.client.render.model.ModelLoader;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.math.AffineTransformation;
 import org.apache.commons.lang3.tuple.Triple;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
@@ -11,14 +16,7 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
-import com.fusionflux.portalcubed.PortalCubed;
-import com.fusionflux.portalcubed.client.render.model.block.EmissiveBakedModel;
-
-import net.minecraft.client.render.model.BakedModel;
-import net.minecraft.client.render.model.ModelBakeSettings;
-import net.minecraft.client.render.model.ModelLoader;
-import net.minecraft.util.Identifier;
-import net.minecraft.util.math.AffineTransformation;
+import java.util.Map;
 
 @Mixin(ModelLoader.class)
 public abstract class ModelLoaderMixin {
@@ -29,10 +27,10 @@ public abstract class ModelLoaderMixin {
 
 	@Inject(method = "bake", at = @At("RETURN"), locals = LocalCapture.CAPTURE_FAILHARD, cancellable = true)
 	private void portalcubed$injectEmissiveModels(Identifier id, ModelBakeSettings settings, CallbackInfoReturnable<BakedModel> cir, Triple<Identifier, AffineTransformation, Boolean> triple) {
-		if (bakedModelCache.get(triple) instanceof EmissiveBakedModel || !id.getNamespace().equals(PortalCubed.MODID)) return;
+		if (bakedModelCache.get(triple) instanceof EmissiveBakedModel || !id.getNamespace().equals(PortalCubed.MOD_ID)) return;
 
 		final BakedModel modelToWrap = cir.getReturnValue();
-		final BakedModel customModel = EmissiveBakedModel.wrap(id, modelToWrap).orElseGet(() -> modelToWrap);
+		final BakedModel customModel = EmissiveBakedModel.wrap(id, modelToWrap).orElse(modelToWrap);
 		if (customModel == null || customModel == modelToWrap) return;
 
 		bakedModelCache.replace(triple, customModel);
