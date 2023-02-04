25f371dce941bf0400dcbb4900c571611baefe1e
Josiah (Gaming32) Glosson <soujournme@gmail.com>
2023-02-03 19:17:27 -0600

    Add hammer

---
+++ src/main/java/com/fusionflux/portalcubed/blocks/SpecialHiddenBlock.java
@@ -0,0 +1,95 @@
+package com.fusionflux.portalcubed.blocks;
+
+import com.fusionflux.portalcubed.items.PortalCubedItems;
+import net.fabricmc.api.EnvType;
+import net.minecraft.block.*;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.network.ClientPlayerEntity;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.fluid.FluidState;
+import net.minecraft.fluid.Fluids;
+import net.minecraft.item.Item;
+import net.minecraft.state.StateManager;
+import net.minecraft.state.property.BooleanProperty;
+import net.minecraft.state.property.Properties;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+import net.minecraft.util.shape.VoxelShape;
+import net.minecraft.util.shape.VoxelShapes;
+import net.minecraft.world.BlockView;
+import net.minecraft.world.WorldAccess;
+import org.quiltmc.loader.api.minecraft.ClientOnly;
+import org.quiltmc.loader.api.minecraft.MinecraftQuiltLoader;
+
+public abstract class SpecialHiddenBlock extends Block implements Waterloggable {
+    public static final BooleanProperty WATERLOGGED = Properties.WATERLOGGED;
+
+    public SpecialHiddenBlock(Settings settings) {
+        super(settings);
+        setDefaultState(
+            getStateManager().getDefaultState()
+                .with(WATERLOGGED, false)
+        );
+    }
+
+    @Override
+    protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
+        builder.add(WATERLOGGED);
+    }
+
+    @Override
+    @SuppressWarnings("deprecation")
+    public VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
+        return isHolding(context, asItem()) || isHolding(context, PortalCubedItems.HAMMER)
+            ? VoxelShapes.fullCube() : VoxelShapes.empty();
+    }
+
+    private static boolean isHolding(ShapeContext context, Item item) {
+        return context.isHolding(item) ||
+            (context instanceof EntityShapeContext entityContext &&
+                entityContext.getEntity() instanceof LivingEntity living &&
+                living.isHolding(item)
+            );
+    }
+
+    @Override
+    public boolean isTranslucent(BlockState state, BlockView world, BlockPos pos) {
+        return true;
+    }
+
+    @Override
+    @SuppressWarnings("deprecation")
+    public BlockRenderType getRenderType(BlockState state) {
+        return MinecraftQuiltLoader.getEnvironmentType() == EnvType.CLIENT
+            ? overrideRenderTypeClient() : BlockRenderType.INVISIBLE;
+    }
+
+    @ClientOnly
+    private BlockRenderType overrideRenderTypeClient() {
+        final ClientPlayerEntity player = MinecraftClient.getInstance().player;
+        return player != null && player.isHolding(PortalCubedItems.HAMMER)
+            ? BlockRenderType.MODEL : BlockRenderType.INVISIBLE;
+    }
+
+    @Override
+    @SuppressWarnings("deprecation")
+    public float getAmbientOcclusionLightLevel(BlockState state, BlockView world, BlockPos pos) {
+        return 1f;
+    }
+
+    @SuppressWarnings("deprecation")
+    @Override
+    public BlockState getStateForNeighborUpdate(BlockState state, Direction direction, BlockState neighborState, WorldAccess world, BlockPos pos, BlockPos neighborPos) {
+        if (state.get(WATERLOGGED)) {
+            world.scheduleFluidTick(pos, Fluids.WATER, Fluids.WATER.getTickRate(world));
+        }
+
+        return super.getStateForNeighborUpdate(state, direction, neighborState, world, pos, neighborPos);
+    }
+
+    @Override
+    @SuppressWarnings("deprecation")
+    public FluidState getFluidState(BlockState state) {
+        return state.get(WATERLOGGED) ? Fluids.WATER.getStill(false) : super.getFluidState(state);
+    }
+}
--- src/main/java/com/fusionflux/portalcubed/mixin/client/ClientPlayerEntityMixin.java
+++ src/main/java/com/fusionflux/portalcubed/mixin/client/ClientPlayerEntityMixin.java
@@ -1,7 +1,9 @@
 package com.fusionflux.portalcubed.mixin.client;
 
 import com.fusionflux.portalcubed.accessor.CalledValues;
+import com.fusionflux.portalcubed.items.PortalCubedItems;
 import com.mojang.authlib.GameProfile;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.AbstractClientPlayerEntity;
 import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.client.world.ClientWorld;
@@ -10,13 +12,19 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.util.shape.VoxelShapes;
 import org.jetbrains.annotations.Nullable;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(ClientPlayerEntity.class)
 public class ClientPlayerEntityMixin extends AbstractClientPlayerEntity {
+
+    @Shadow @Final protected MinecraftClient client;
+    private boolean portalCubed$wasHoldingHammer = false;
 
     public ClientPlayerEntityMixin(ClientWorld clientWorld, GameProfile gameProfile, @Nullable PlayerPublicKey playerPublicKey) {
         super(clientWorld, gameProfile, playerPublicKey);
@@ -28,6 +36,15 @@ public class ClientPlayerEntityMixin extends AbstractClientPlayerEntity {
         VoxelShape portalBox = CalledValues.getPortalCutout(((ClientPlayerEntity)(Object)this));
         if(portalBox != VoxelShapes.empty()){
             cir.setReturnValue(false);
+        }
+    }
+
+    @Inject(method = "tick", at = @At("TAIL"))
+    private void portalCubed$onTick(CallbackInfo ci) {
+        final boolean isHoldingHammer = isHolding(PortalCubedItems.HAMMER);
+        if (isHoldingHammer != portalCubed$wasHoldingHammer) {
+            portalCubed$wasHoldingHammer = isHoldingHammer;
+            client.worldRenderer.reload();
         }
     }
 
