fa8d34204c9947dfcc65ebffa18af404fe7268f6
Josiah (Gaming32) Glosson <soujournme@gmail.com>
2023-02-17 14:24:58 -0600

    Fix pellets having a forward velocity

--- src/main/java/com/fusionflux/portalcubed/items/EnergyPelletItem.java
+++ src/main/java/com/fusionflux/portalcubed/items/EnergyPelletItem.java
@@ -7,6 +7,8 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
+import net.minecraft.util.math.Direction;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 
 public class EnergyPelletItem extends Item {
@@ -24,7 +26,11 @@ public class EnergyPelletItem extends Item {
         final EnergyPelletEntity pellet = PortalCubedEntities.ENERGY_PELLET.create(world);
         if (pellet == null) return TypedActionResult.pass(item);
         pellet.setPosition(user.getEyePos().add(user.getRotationVector()));
-        pellet.setVelocity(user.getVelocity().add(user.getRotationVector().multiply(0.25)));
+        Vec3d userVelocity = user.getVelocity();
+        if (user.isOnGround()) {
+            userVelocity = userVelocity.withAxis(Direction.Axis.Y, 0);
+        }
+        pellet.setVelocity(userVelocity.add(user.getRotationVector().multiply(0.25)));
         if (user.isSneaking()) {
             pellet.resetLife(-1);
         }
