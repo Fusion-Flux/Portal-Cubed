114f1ff1cf43d977a713993a9b82878911b78ead
Josiah (Gaming32) Glosson <soujournme@gmail.com>
2023-02-16 22:19:15 -0600

    Pellet behavior somewhat

--- src/main/java/com/fusionflux/portalcubed/client/render/entity/EnergyPelletRenderer.java
+++ src/main/java/com/fusionflux/portalcubed/client/render/entity/EnergyPelletRenderer.java
@@ -14,9 +14,13 @@ import net.minecraft.util.Identifier;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.util.math.Vec3f;
+import org.jetbrains.annotations.Nullable;
 
 public class EnergyPelletRenderer extends EntityRenderer<EnergyPelletEntity> {
     private static final ItemStack ITEM = new ItemStack(PortalCubedItems.ENERGY_PELLET);
+
+    @Nullable
+    public static Float pelletAlpha = null;
 
     public EnergyPelletRenderer(EntityRendererFactory.Context ctx) {
         super(ctx);
@@ -39,8 +43,12 @@ public class EnergyPelletRenderer extends EntityRenderer<EnergyPelletEntity> {
             (float)Math.toDegrees(MathHelper.atan2(diff.y, MathHelper.sqrt((float) (diff.x * diff.x + diff.z * diff.z))))
         ));
 
+        if (entity.getStartingLife() > 0) {
+            pelletAlpha = MathHelper.clamp(MathHelper.lerp((float)entity.getLife() / entity.getStartingLife(), 0.25f, 1f), 0f, 1f);
+        }
         mc.getItemRenderer()
             .renderItem(ITEM, ModelTransformation.Mode.GROUND, light, OverlayTexture.DEFAULT_UV, matrices, vertexConsumers, 0);
+        pelletAlpha = null;
         matrices.pop();
     }
 
--- src/main/java/com/fusionflux/portalcubed/entity/EnergyPelletEntity.java
+++ src/main/java/com/fusionflux/portalcubed/entity/EnergyPelletEntity.java
@@ -1,17 +1,28 @@
 package com.fusionflux.portalcubed.entity;
 
+import com.fusionflux.portalcubed.config.PortalCubedConfig;
+import com.fusionflux.portalcubed.mechanics.PortalCubedDamageSources;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.MovementType;
+import net.minecraft.entity.ai.TargetPredicate;
 import net.minecraft.entity.data.DataTracker;
 import net.minecraft.entity.data.TrackedData;
 import net.minecraft.entity.data.TrackedDataHandlerRegistry;
+import net.minecraft.entity.projectile.ProjectileUtil;
 import net.minecraft.nbt.NbtCompound;
 import net.minecraft.network.Packet;
 import net.minecraft.network.packet.s2c.play.EntitySpawnS2CPacket;
+import net.minecraft.util.hit.EntityHitResult;
+import net.minecraft.util.hit.HitResult;
+import net.minecraft.util.math.Direction;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 
 public class EnergyPelletEntity extends Entity {
-    private static final TrackedData<Float> SPEED = DataTracker.registerData(EnergyPelletEntity.class, TrackedDataHandlerRegistry.FLOAT);
+    private static final TrackedData<Integer> STARTING_LIFE = DataTracker.registerData(EnergyPelletEntity.class, TrackedDataHandlerRegistry.INTEGER);
+    private static final TrackedData<Integer> LIFE = DataTracker.registerData(EnergyPelletEntity.class, TrackedDataHandlerRegistry.INTEGER);
 
     public EnergyPelletEntity(EntityType<?> type, World world) {
         super(type, world);
@@ -19,17 +30,18 @@ public class EnergyPelletEntity extends Entity {
 
     @Override
     protected void initDataTracker() {
-        dataTracker.startTracking(SPEED, 1f);
+        dataTracker.startTracking(STARTING_LIFE, 220);
+        dataTracker.startTracking(LIFE, 220);
     }
 
     @Override
     protected void readCustomDataFromNbt(NbtCompound nbt) {
-        setSpeed(nbt.getFloat("Speed"));
+        setLife(nbt.getInt("Life"));
     }
 
     @Override
     protected void writeCustomDataToNbt(NbtCompound nbt) {
-        nbt.putFloat("Speed", getSpeed());
+        nbt.putInt("Life", getLife());
     }
 
     @Override
@@ -37,11 +49,73 @@ public class EnergyPelletEntity extends Entity {
         return new EntitySpawnS2CPacket(this);
     }
 
-    public float getSpeed() {
-        return dataTracker.get(SPEED);
+    public int getStartingLife() {
+        return dataTracker.get(STARTING_LIFE);
     }
 
-    public void setSpeed(float speed) {
-        dataTracker.set(SPEED, speed);
+    public void setStartingLife(int ticks) {
+        dataTracker.set(STARTING_LIFE, ticks);
+    }
+
+    public int getLife() {
+        return dataTracker.get(LIFE);
+    }
+
+    public void setLife(int ticks) {
+        dataTracker.set(LIFE, ticks);
+    }
+
+    @Override
+    public void tick() {
+        super.tick();
+        if (world.isClient) return;
+        Vec3d vel = getVelocity();
+        move(MovementType.SELF, getVelocity());
+        int life = getLife();
+        if (life > 0) {
+            setLife(--life);
+        } else if (life == 0) {
+            // TODO: Death effects, etc.
+            kill();
+        } // life < 0 means green pellet
+        if (verticalCollision) {
+            setVelocity(vel = getVelocity().withAxis(Direction.Axis.Y, -vel.y));
+        }
+        if (horizontalCollision) {
+            if (getVelocity().x == 0) {
+                setVelocity(vel = getVelocity().withAxis(Direction.Axis.X, -vel.x));
+            }
+            if (getVelocity().z == 0) {
+                setVelocity(getVelocity().withAxis(Direction.Axis.Z, -vel.z));
+            }
+        }
+        final HitResult hit = ProjectileUtil.getCollision(this, this::canHit);
+        if (hit.getType() == HitResult.Type.ENTITY) {
+            kill((LivingEntity)((EntityHitResult)hit).getEntity());
+        } else {
+            final LivingEntity hit2 = world.getClosestEntity(
+                LivingEntity.class,
+                TargetPredicate.createNonAttackable().setPredicate(this::canHit),
+                null, getX(), getY(), getZ(),
+                getBoundingBox()
+            );
+            if (hit2 != null) {
+                kill(hit2);
+            }
+        }
+    }
+
+    protected boolean canHit(Entity entity) {
+        return entity instanceof LivingEntity && !entity.isSpectator() && entity.isAlive() && entity.collides();
+    }
+
+    private void kill(LivingEntity entity) {
+        entity.damage(PortalCubedDamageSources.VAPORIZATION, PortalCubedConfig.pelletDamage);
+        kill();
+    }
+
+    @Override
+    public boolean shouldRender(double distance) {
+        return true;
     }
 }
