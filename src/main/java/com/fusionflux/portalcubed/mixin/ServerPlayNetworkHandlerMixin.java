7dbde94c49380a4275ea93a4ec4f85d226d21920
maximumpower55 <maximumpowermc@gmail.com>
2023-02-19 15:10:53 -0800

    why

--- src/main/java/com/fusionflux/portalcubed/mixin/ServerPlayNetworkHandlerMixin.java
+++ src/main/java/com/fusionflux/portalcubed/mixin/ServerPlayNetworkHandlerMixin.java
@@ -2,14 +2,20 @@ package com.fusionflux.portalcubed.mixin;
 
 import com.fusionflux.portalcubed.accessor.CalledValues;
 import com.fusionflux.portalcubed.accessor.CustomCollisionView;
+import com.fusionflux.portalcubed.entity.ExperimentalPortal;
+
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.projectile.ProjectileUtil;
 import net.minecraft.server.network.ServerPlayNetworkHandler;
 import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.util.shape.VoxelShapes;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
 
 @Mixin(ServerPlayNetworkHandler.class)
@@ -28,6 +34,21 @@ public abstract class ServerPlayNetworkHandlerMixin {
         }
 
         return shapes;
+    }
+
+    @Redirect(method = "onPlayerInteractBlock", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/math/Vec3d;squaredDistanceTo(Lnet/minecraft/util/math/Vec3d;)D", ordinal = 0))
+    private double portalCubed$crossPortalInteractionAntiCheatFix1(Vec3d from, Vec3d to) {
+        return portalCubed$portalCompatibleSquaredDistanceTo(from, to, ServerPlayNetworkHandler.MAX_INTERACTION_DISTANCE);
+    }
+
+    private double portalCubed$portalCompatibleSquaredDistanceTo(Vec3d from, Vec3d to, double maxRayDistance) {
+        final var end = player.getRotationVector().multiply(maxRayDistance/maxRayDistance);
+        final var box = player.getBoundingBox().stretch(end).expand(1d);
+        final var hit = ProjectileUtil.raycast(player, player.getEyePos(), player.getEyePos().add(end), box, e -> e instanceof ExperimentalPortal, maxRayDistance);
+        if (hit != null && hit.getEntity() instanceof ExperimentalPortal portal && portal.getDestination().isPresent()) {
+            return portal.getDestination().get().add(0, player.getEyeY(), 0).squaredDistanceTo(to);
+        }
+        return from.squaredDistanceTo(to);
     }
 
 }
