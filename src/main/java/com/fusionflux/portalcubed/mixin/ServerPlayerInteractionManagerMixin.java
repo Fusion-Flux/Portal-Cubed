fe00e5300375817129f571c8c312cc75f750f66d
Josiah (Gaming32) Glosson <soujournme@gmail.com>
2023-03-02 17:40:54 -0600

    Fix Pehkui compat

--- src/main/java/com/fusionflux/portalcubed/mixin/ServerPlayerInteractionManagerMixin.java
+++ src/main/java/com/fusionflux/portalcubed/mixin/ServerPlayerInteractionManagerMixin.java
@@ -11,12 +11,19 @@ import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
-@Mixin(ServerPlayerInteractionManager.class)
+@Mixin(value = ServerPlayerInteractionManager.class, priority = 1001)
 public abstract class ServerPlayerInteractionManagerMixin {
 
     @Shadow @Final protected ServerPlayerEntity player;
 
-    @Redirect(method = "processBlockBreakingAction", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/math/Vec3d;squaredDistanceTo(Lnet/minecraft/util/math/Vec3d;)D", ordinal = 0))
+    @Redirect(
+        method = "processBlockBreakingAction",
+        at = @At(
+            value = "INVOKE",
+            target = "Lnet/minecraft/util/math/Vec3d;squaredDistanceTo(Lnet/minecraft/util/math/Vec3d;)D",
+            ordinal = 0
+        )
+    )
     private double portalCubed$replaceWithCrossPortalInteractionDistanceCheck4(Vec3d from, Vec3d to) {
         return CrossPortalInteraction.interactionDistance(player, ServerPlayNetworkHandler.MAX_INTERACTION_DISTANCE, to);
     }
