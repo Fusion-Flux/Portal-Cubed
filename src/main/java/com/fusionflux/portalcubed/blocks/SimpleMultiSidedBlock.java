36c9c12c832c1325afd9f688898c75c94f55a739
woodiertexas <woodietexas@protonmail.com>
2023-03-27 16:58:10 -0700

    Checkstyle stuff

--- src/main/java/com/fusionflux/portalcubed/blocks/SimpleMultiSidedBlock.java
+++ src/main/java/com/fusionflux/portalcubed/blocks/SimpleMultiSidedBlock.java
@@ -9,14 +9,14 @@ import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.util.shape.VoxelShapes;
 import net.minecraft.world.BlockView;
 
-public class SimpleMultiSidedBlock extends AbstractLichenBlock {    
+public class SimpleMultiSidedBlock extends AbstractLichenBlock {
     public SimpleMultiSidedBlock(Settings settings) {
         super(settings);
     }
-    
+
     @Override
     public VoxelShape getCollisionShape(BlockState state, BlockView view, BlockPos pos, ShapeContext context) {
-	return VoxelShapes.empty();
+         return VoxelShapes.empty();
     }
 
     @Override
