302f639a6a0b087def1578b6c4537fd88b50ff74
woodiertexas <woodietexas@protonmail.com>
2023-03-27 14:44:20 -0700

    Add imports lol

--- src/main/java/com/fusionflux/portalcubed/blocks/SimpleMultiSidedBlock.java
+++ src/main/java/com/fusionflux/portalcubed/blocks/SimpleMultiSidedBlock.java
@@ -1,7 +1,13 @@
 package com.fusionflux.portalcubed.blocks;
 
 import net.minecraft.block.AbstractLichenBlock;
+import net.minecraft.block.BlockState;
 import net.minecraft.block.LichenSpreadBehavior;
+import net.minecraft.block.ShapeContext;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.shape.VoxelShape;
+import net.minecraft.util.shape.VoxelShapes;
+import net.minecraft.world.BlockView;
 
 public class SimpleMultiSidedBlock extends AbstractLichenBlock {
     protected static final VoxelShape NO_COLLISION = VoxelShapes.empty();
