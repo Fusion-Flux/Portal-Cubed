d330036854dd261c58bcdaaf742d4d4460ea5dff
woodiertexas <woodietexas@protonmail.com>
2023-03-27 16:35:11 -0700

    VoxelShapes.empty() is already cached apparently.

--- src/main/java/com/fusionflux/portalcubed/blocks/SimpleMultiSidedBlock.java
+++ src/main/java/com/fusionflux/portalcubed/blocks/SimpleMultiSidedBlock.java
@@ -9,16 +9,14 @@ import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.util.shape.VoxelShapes;
 import net.minecraft.world.BlockView;
 
-public class SimpleMultiSidedBlock extends AbstractLichenBlock {
-    protected static final VoxelShape NO_COLLISION = VoxelShapes.empty();
-    
+public class SimpleMultiSidedBlock extends AbstractLichenBlock {    
     public SimpleMultiSidedBlock(Settings settings) {
         super(settings);
     }
     
     @Override
     public VoxelShape getCollisionShape(BlockState state, BlockView view, BlockPos pos, ShapeContext context) {
-	return NO_COLLISION;
+	return VoxelShapes.empty();
     }
 
     @Override
