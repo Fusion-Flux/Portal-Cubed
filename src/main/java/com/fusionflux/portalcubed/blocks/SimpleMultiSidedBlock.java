1a5613d34acc32dd4154833ab0bf3b7c69bf0b37
woodiertexas <woodietexas@protonmail.com>
2023-03-27 14:09:41 -0700

    Make the facades have no collision.

--- src/main/java/com/fusionflux/portalcubed/blocks/SimpleMultiSidedBlock.java
+++ src/main/java/com/fusionflux/portalcubed/blocks/SimpleMultiSidedBlock.java
@@ -4,9 +4,16 @@ import net.minecraft.block.AbstractLichenBlock;
 import net.minecraft.block.LichenSpreadBehavior;
 
 public class SimpleMultiSidedBlock extends AbstractLichenBlock {
+    protected static final VoxelShape NO_COLLISION = VoxelShapes.empty();
+    
     public SimpleMultiSidedBlock(Settings settings) {
         super(settings);
     }
+    
+    @Override
+	public VoxelShape getOutlineShape(BlockState state, BlockView view, BlockPos pos, ShapeContext context) {
+		return NO_COLLISION;
+	}
 
     @Override
     public LichenSpreadBehavior getLichenSpreadBehavior() {
