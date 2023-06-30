31277307aa930674b9ce10d06c7ad90dc860495e
Josiah (Gaming32) Glosson <soujournme@gmail.com>
2023-06-30 08:12:31 -0500

    Copy BlockBehaviour.canBeReplaced to FacadeBlock

--- src/main/java/com/fusionflux/portalcubed/blocks/FacadeBlock.java
+++ src/main/java/com/fusionflux/portalcubed/blocks/FacadeBlock.java
@@ -1,20 +1,29 @@
 package com.fusionflux.portalcubed.blocks;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.Shapes;
 import net.minecraft.world.phys.shapes.VoxelShape;
+import org.jetbrains.annotations.NotNull;
 
 public class FacadeBlock extends SimpleMultiSidedBlock {
     public FacadeBlock(Properties settings) {
         super(settings);
     }
 
+    @NotNull
     @Override
     @SuppressWarnings("deprecation")
     public VoxelShape getCollisionShape(BlockState state, BlockGetter world, BlockPos pos, CollisionContext context) {
         return Shapes.empty();
+    }
+
+    @Override
+    public boolean canBeReplaced(BlockState state, BlockPlaceContext useContext) {
+        // Copy from BlockBehaviour, instead of MultifaceBlock
+        return state.canBeReplaced() && (useContext.getItemInHand().isEmpty() || !useContext.getItemInHand().is(this.asItem()));
     }
 }
