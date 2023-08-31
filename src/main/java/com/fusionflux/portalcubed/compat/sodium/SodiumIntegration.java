7336b9822b282c791302c2242d05d39d3e4dfd90
TropheusJ <jverrellijr@icloud.com>
2023-08-31 01:03:57 -0400

    update and futureproof sodium compat

--- src/main/java/com/fusionflux/portalcubed/compat/sodium/SodiumIntegration.java
+++ src/main/java/com/fusionflux/portalcubed/compat/sodium/SodiumIntegration.java
@@ -1,12 +1,28 @@
 package com.fusionflux.portalcubed.compat.sodium;
 
+import com.fusionflux.portalcubed.PortalCubed;
 import com.fusionflux.portalcubed.compat.sodium.absent.SodiumIntegrationAbsent;
 import com.fusionflux.portalcubed.compat.sodium.present.SodiumIntegrationPresent;
+
+import net.minecraft.Util;
 import net.minecraft.client.renderer.texture.SpriteContents;
 import org.quiltmc.loader.api.QuiltLoader;
 
 public interface SodiumIntegration {
-    SodiumIntegration INSTANCE = QuiltLoader.isModLoaded("sodium") ? SodiumPresentHolder.create() : SodiumIntegrationAbsent.INSTANCE;
+    SodiumIntegration INSTANCE = Util.make(() -> {
+        if (!QuiltLoader.isModLoaded("sodium")) {
+            return SodiumIntegrationAbsent.INSTANCE;
+        }
+
+       try {
+           Class<?> spriteContentsEx = Class.forName("me.jellysquid.mods.sodium.client.render.texture.SpriteContentsExtended");
+           spriteContentsEx.getDeclaredMethod("sodium$setActive", Boolean.TYPE);
+           return SodiumPresentHolder.create();
+       } catch (ClassNotFoundException | NoSuchMethodException e) {
+           PortalCubed.LOGGER.error("Portal Cubed has outdated Sodium compatibility! Some things may not work properly.");
+           return SodiumIntegrationAbsent.INSTANCE;
+       }
+    });
 
     void markSpriteActive(SpriteContents sprite);
 
--- src/main/java/com/fusionflux/portalcubed/compat/sodium/present/SodiumIntegrationPresent.java
+++ src/main/java/com/fusionflux/portalcubed/compat/sodium/present/SodiumIntegrationPresent.java
@@ -1,14 +1,15 @@
 package com.fusionflux.portalcubed.compat.sodium.present;
 
 import com.fusionflux.portalcubed.compat.sodium.SodiumIntegration;
-import me.jellysquid.mods.sodium.client.render.texture.SpriteExtended;
+import me.jellysquid.mods.sodium.client.render.texture.SpriteContentsExtended;
+
 import net.minecraft.client.renderer.texture.SpriteContents;
 
 public class SodiumIntegrationPresent implements SodiumIntegration {
     @Override
     public void markSpriteActive(SpriteContents sprite) {
-        if (sprite instanceof SpriteExtended extended) {
-            extended.setActive(true);
+        if (sprite instanceof SpriteContentsExtended extended) {
+            extended.sodium$setActive(true);
         }
     }
 }
